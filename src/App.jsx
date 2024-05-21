cfcf9c774f92ae8c9e0b2cc3233723dddca20980
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-05-21 14:45:45 +0530

    feat: Carousel for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="">DonateEase</div>;
+  return (
+    <div className="">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
